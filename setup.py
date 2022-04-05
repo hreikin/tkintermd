f1bffe08106365faa8d185b18a5ee8701ef7c951
Michael Haslam <hreikin@gmail.com>
2022-04-05 11:48:42 +0100

    Update version numbers.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="tkintermd",
-    version="0.0.3",
+    version="0.0.4",
     description="A Python based Markdown editor with HTML preview",
     long_description=README,
     long_description_content_type="text/markdown",
--- tkintermd/__init__.py
+++ tkintermd/__init__.py
@@ -1,2 +1,2 @@
 # Version of tkintermd package
-__version__ = "0.0.3"
+__version__ = "0.0.4"
