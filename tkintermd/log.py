afd3d1ca7065f11ebaab67531559e166b5514c6e
Michael Haslam <hreikin@gmail.com>
2022-04-09 22:57:56 +0100

    Create logger function to instantiate a logger and return it.

---
+++ tkintermd/log.py
@@ -0,0 +1,42 @@
+import logging
+
+##################################### LOGS #####################################
+# Initialize the logger and specify the level of logging. This will log "DEBUG" 
+# and higher messages to file and log "WARNING" and higher messages to the console.
+
+def create_logger():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='[%(levelname)s][%(name)s][%(asctime)s]: %(message)s',
+                        datefmt='%d-%m-%y-%H:%M:%S',
+                        filename='debug.log',
+                        filemode='a')
+
+    # Define a "handler" which writes "WARNING" messages or higher to the "sys.stderr".
+    console = logging.StreamHandler()
+    console.setLevel(logging.WARNING)
+
+    # Set a format which is simpler for console messages.
+    formatter = logging.Formatter('[%(levelname)s][%(asctime)s]: %(message)s', datefmt='%H:%M:%S')
+
+    # Tell the console "handler" to use this format.
+    console.setFormatter(formatter)
+
+    # Add the "handler" to the "root logger".
+    log = logging.getLogger(__name__)
+    log.addHandler(console)
+
+    return log
+
+# Example Usage
+#
+#     import tkintermd.log as logg
+#
+#     logger = log.create_logger()
+#     logger.debug("DEBUG")
+#     logger.info("INFO")
+#     logger.exception("EXCEPTION")
+#     logger.warning("WARNING")
+#     logger.error("ERROR")
+#     logger.critical("CRITICAL")
+
+################################################################################
